c53033bb9f1118c645753d222cd52cd1b268968a
MayumiR <mayumikarashmi@gmail.com>
2018-06-15 21:49:24 +0530

    DTO

--- src/lk/ijse/payroll/model/AttendanceDTO.java
+++ src/lk/ijse/payroll/model/AttendanceDTO.java
@@ -18,15 +18,9 @@ public class AttendanceDTO {
     private int OThrs,lateHrs;
     private String inTime;
     private String outTime;
-  private int count;
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
+    private int leaveCount;
+    private int presentCount;
+  
     public AttendanceDTO() {
     }
 
@@ -40,6 +34,22 @@ public class AttendanceDTO {
         this.lateHrs = lateHrs;
         this.inTime = inTime;
         this.outTime = outTime;
+    }
+
+    public int getLeaveCount() {
+        return leaveCount;
+    }
+
+    public void setLeaveCount(int leaveCount) {
+        this.leaveCount = leaveCount;
+    }
+
+    public int getPresentCount() {
+        return presentCount;
+    }
+
+    public void setPresentCount(int presentCount) {
+        this.presentCount = presentCount;
     }
 
     /**
